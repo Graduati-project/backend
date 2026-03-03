0a648ad247439559d06c47e97bed7f6ecb7b56aa
abdo <bkra32887@gmail.com>
2026-03-03 17:28:56 +0200

    remove

--- src/modules/user/user.validation.js
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-import { generalFields } from "../../middleware/validation.js";
-
-export const shareProfile = {
-  params: joi.object().keys({
-    userId: generalFields.userId.required(),
-  }),
-};
